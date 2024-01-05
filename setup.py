2bc28830224d471657ebc55fe39611ff9ede005a
David Ansa <davitacols@gmail.com>
2024-01-05 12:40:47 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='dataDisk',
-    version='1.1.0',
-    url='https://github.com/davitacols/DataFlow',
-    packages=find_packages(),
-    install_requires=[
-        'concurrent.futures',
-        'multiprocessing',
-    ],
-    extras_require={
-        'data_processing': ['numpy', 'pandas'],
-    },
-)
+Metadata-Version: 2.1
+Name: dataDisk
+Version: 1.1.0
+Home-page: https://github.com/davitacols/dataDisk
+Classifier: License :: OSI Approved :: MIT License
+Classifier: Programming Language :: Python :: 3
+Classifier: Programming Language :: Python :: 3.6
+Classifier: Programming Language :: Python :: 3.7
+Classifier: Programming Language :: Python :: 3.8
+Classifier: Programming Language :: Python :: 3.9
+Classifier: Programming Language :: Python :: 3.10
+Requires-Python: >=3.6
+License-File: LICENCE.md
+Requires-Dist: concurrent.futures
+Provides-Extra: data-processing
+Requires-Dist: numpy; extra == "data-processing"
+Requires-Dist: pandas; extra == "data-processing"
