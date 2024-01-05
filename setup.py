68e0c0fadba7ee03f7241963cf7019b9006a43de
davitacols <98448386+davitacols@users.noreply.github.com>
2024-01-05 13:16:11 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='dataDisk',
     version='1.1.0',
-    url='https://github.com/davitacols/DataFlow',
+    url='https://github.com/davitacols/dataDisk',
     packages=find_packages(),
     install_requires=[
         'concurrent.futures',
-        'multiprocessing',
     ],
     extras_require={
         'data_processing': ['numpy', 'pandas'],
     },
+    python_requires='>=3.6',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
