efb689c92106783ff728080dbae06408da9d3adf
davitacols <98448386+davitacols@users.noreply.github.com>
2024-01-05 10:53:14 +0100

    Remove unimplemented functionality

--- dataDisk/__init__.py
+++ dataDisk/__init__.py
@@ -2,4 +2,3 @@ from .pipeline import DataPipeline  # noqa
 from .transformation import Transformation  # noqa
 from .validator import Validator  # noqa
 from .parallel_processor import ParallelProcessor  # noqa
-from .visual_builder import VisualPipelineBuilder, Task  # noqa
